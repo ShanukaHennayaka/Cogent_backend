71cd5b9c77103d9c4be7c07fac366f35118f0e40
ShanukaHennayaka <shanukasupun25@gmail.com>
2025-04-11 10:11:22 +0530

    update

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const registerRoutes = require('./api/routes/registerRoutes');
+const dotenv = require('dotenv');
+const registerRoutes = require('./routes/registerRoutes');
+
+dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
+app.use(cors());      
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((error) => console.error('Error connecting to MongoDB', error));
 
-// Routes
 app.use('/api', registerRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
